8464b7be4dec6738291fab567af8a85508b15ba7
Jose <jvrs.90@gmail.com>
2023-05-14 19:43:37 +0200

    styles: home changed

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { prisma } from "@/lib/prisma";
-import React from "react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+
 
 export default function RegisterPage() {
   const { register, handleSubmit } = useForm();
+  const { replace } = useRouter();
 
   const onSubmit = async (data: any) => {
     await fetch("/api/register", {
@@ -12,55 +13,62 @@ export default function RegisterPage() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
+
+    replace("/api/auth/signin");
+    
   };
 
   return (
-    <div className="w-2/3 mx-auto">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-5">
-          <label className="block font-semibold" htmlFor="name">
-            Nombre
-          </label>
-          <input
-            className="p-2 w-full rounded shadow"
-            type="text"
-            id="name"
-            {...register("name")}
-          />
+    <section className="bg-fuchsia-100 min-h-screen flex items-center justify-center flex-col px-5 ">
+      <h1 className="text-3xl font-bold text-gray-900 mb-5" >Registro</h1>
+      <div className="mx-auto bg-white p-3 rounded-xl shadow-xl shadow-fuchsia-200/50 w-full md:w-1/2">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-5">
+            <label className="block font-semibold" htmlFor="name">
+              Nombre
+            </label>
+            <input
+              className="p-2 w-full border border-fuchsia-200/40 rounded"
+              type="text"
+              id="name"
+              {...register("name")}
+            />
+          </div>
+          <div className="mb-5">
+            <label className="block font-semibold" htmlFor="email">
+              Correo electronico
+            </label>
+            <input
+              className="p-2 w-full border border-fuchsia-200/40 rounded"
+              type="text"
+              id="email"
+              {...register("email")}
+            />
+          </div>
+          <div className="mb-5">
+            <label className="block font-semibold" htmlFor="email">
+              Password
+            </label>
+            <input
+              className="p-2 w-full border border-fuchsia-200/40 rounded"
+              type="password"
+              id="password"
+              {...register("password")}
+            />
+          </div>
+          <div className="flex justify-end w-full">
+            <button
+              className="bg-fuchsia-800 rounded  px-3 py-2 text-white font-semibold hover:bg-fuchsia-900"
+              type="submit"
+            >
+              Registrarse
+            </button>
+          </div>
+        </form>
+        <div className="mt-5 flex justify-end">
+          <a href="/api/auth/signin">¿Ya tienes cuenta?</a>
         </div>
-        <div className="mb-5">
-          <label className="block font-semibold" htmlFor="email">
-            Correo electronico
-          </label>
-          <input
-            className="p-2 w-full rounded shadow"
-            type="text"
-            id="email"
-            {...register("email")}
-          />
-        </div>
-        <div className="mb-5">
-          <label className="block font-semibold" htmlFor="email">
-            Password
-          </label>
-          <input
-            className="p-2 w-full rounded shadow"
-            type="password"
-            id="password"
-            {...register("password")}
-          />
-        </div>
-        <button
-          className="bg-blue-500 rounded px-3 py-2 text-white hover:bg-blue-600"
-          type="submit"
-        >
-          Registrarse
-        </button>
-      </form>
-
-      <div className="mt-5">
-        <a href="/api/auth/signin">Iniciar sesion</a>
       </div>
-    </div>
+    </section>
   );
 }
